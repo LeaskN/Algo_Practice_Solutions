dafd2481a7c3bc88fb8eee6334cf91d0a752d327
Nicholas Leask <leask.nicholas@gmail.com>
2018-08-30 10:17:11 -0400

    mumbling

---
+++ mumbling.js
@@ -0,0 +1,12 @@
+function accum(s) {
+	let string = s.split('');
+  for(let i = 0; i < string.length; i++) {
+    string[i] = string[i].toUpperCase() + string[i].toLowerCase().repeat(i);
+  }
+  return string.join('-');
+}
+
+console.log((accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"));
+	console.log((accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"));
+	console.log((accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"));
+	console.log((accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"));
