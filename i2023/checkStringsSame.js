1feb44109f011aad8049471af1394ee373a9eb55
Nicholas Leask <leask.nicholas@gmail.com>
2023-05-26 12:12:30 -0400

    checkStringSame

--- i2023/checkStringsSame.js
+++ i2023/checkStringsSame.js
@@ -1,10 +1,26 @@
+/*
+    Given two strings check if they have exactly the same characters
+    The characters do not have to be in the same place
+*/
 function stringCheck(str1, str2){
-    console.log(makeMap(str1), makeMap(str2))
+    if(str1.length != str2.length)return false;
+
+    let map = {};
+    let finish = false;
+    
+    for(let i = 0; i < str1.length; i++){
+        map[str1[i]] ? map[str1[i]]++ : map[str1[i]] = 1; 
+    }
+    
+    for(let i = 0; i < str2.length; i++){
+        map[str2[i]] ? map[str2[i]]-- : finish = true; 
+        if(finish) return false;
+    }
+
+    return true;
 }
 
-function makeMap(str){
-    let output = new Object(str.split(''));
-    console.log(output);
-}
-
-console.log(stringCheck('thisisastring', 'astringthisis'))
+console.log(stringCheck('thisisastring', 'astringthisis'));
+console.log(stringCheck('thisisastring', 'astringthisisa'));
+console.log(stringCheck('thisisastrings', 'astringthisis'));
+console.log(stringCheck('abcde', 'abcx'));
